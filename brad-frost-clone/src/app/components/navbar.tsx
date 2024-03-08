af8451a079d9728157254d1f2dc5767f0c9b5fdd
kristopher french <kristopher.t.french.civ@navseabrokerage.onmicrosoft.com>
2024-03-08 14:52:30 +0900

    Working Navbar component

--- brad-frost-clone/src/app/components/navbar.tsx
+++ brad-frost-clone/src/app/components/navbar.tsx
@@ -3,7 +3,19 @@ import Link from "next/link";
 
 const Navbar: React.FC = () => {
     return (
-        <nav>NAVBAR</nav>
+        <header className="flex flex-col items-end absolute top-0 right-0 z-10 mt-4 p-1">
+            <Link href={`/`} rel="brad home">
+              <span>my name is</span><br/>
+              <span>brad frost</span> 
+            </Link>
+            <nav className="space-y-2 mt-4">
+              <Link href={`/work`}>work</Link>
+              <Link href={`/book`}>book</Link>
+              <Link href={`/blog`}>blog</Link>
+              <Link href={`/contact`}></Link>
+            </nav>
+        </header>
+        
     );
 };
 
